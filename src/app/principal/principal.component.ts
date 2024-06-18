debecbcc713475c68c1c1389ed89172587de87e3
knarfangelo <knarf2003angelo@gmail.com>
2024-06-17 20:40:04 -0500

    version 2 group

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -9,5 +9,31 @@ import { RouterLink } from '@angular/router';
   styleUrl: './principal.component.css'
 })
 export class PrincipalComponent {
+  @ViewChild('consultantsVideo') consultantsVideoRef!: ElementRef<HTMLVideoElement>;
+  @ViewChild('expressVideo') expressVideoRef!: ElementRef<HTMLVideoElement>;
+  @ViewChild('industryVideo') industryVideoRef!: ElementRef<HTMLVideoElement>;
 
+  private previewTimeout: any;
+
+  startPreview(video: HTMLVideoElement) {
+    video.muted = true;
+    video.currentTime = 1;
+    video.playbackRate = 0.5;
+    video.play();
+  }
+
+  stopPreview() {
+    if (this.previewTimeout) {
+      clearTimeout(this.previewTimeout);
+    }
+    this.consultantsVideoRef.nativeElement.currentTime = 0;
+    this.expressVideoRef.nativeElement.currentTime = 0;
+    this.industryVideoRef.nativeElement.currentTime = 0;
+    this.consultantsVideoRef.nativeElement.playbackRate = 1;
+    this.expressVideoRef.nativeElement.playbackRate = 1;
+    this.industryVideoRef.nativeElement.playbackRate = 1;
+    this.consultantsVideoRef.nativeElement.pause();
+    this.expressVideoRef.nativeElement.pause();
+    this.industryVideoRef.nativeElement.pause();
+  }
 }
