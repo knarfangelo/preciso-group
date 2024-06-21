db9233de395eb0688f64607947209c03df570255
knarfangelo <knarf2003angelo@gmail.com>
2024-06-21 18:35:01 -0500

    Icono de asistente agregado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,9 @@ import { Routes } from '@angular/router';
 import { PrincipalComponent } from './principal/principal.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { ServiciosComponent } from './components/servicios/servicios.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
 export const routes: Routes = [
-    {path:'nosotros', component:PrincipalComponent},
+    {path:'nosotros', component:NosotrosComponent},
     {path:'', component:PrincipalComponent},
     {path:'inicio', component:InicioComponent},
     {path:'servicios', component:ServiciosComponent},
