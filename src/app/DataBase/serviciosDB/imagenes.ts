e9aa40e641e51578e0ddc37765040dba3df40be6
knarfangelo <knarf2003angelo@gmail.com>
2024-06-27 17:50:44 -0500

    Avanze grupo v2

---
+++ src/app/DataBase/serviciosDB/imagenes.ts
@@ -0,0 +1,10 @@
+export const galeriaImagenes:string[]=[
+    "/galeria/1.jpg",
+    "/galeria/2.jpg",
+    "/galeria/3.jpg",
+    "/galeria/4.jpg",
+    "/galeria/5.jpg",
+    "/galeria/6.jpg",
+    "/galeria/7.jpg",
+    "/galeria/8.jpg",
+]
--- src/app/components/galeria/galeria.component.ts
+++ src/app/components/galeria/galeria.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, signal } from '@angular/core';
+import { SwiperOptions } from 'swiper/types';
+import { EffectCube } from 'swiper/modules';
+import { SwiperContainer, register } from 'swiper/element';
+import { galeriaImagenes } from '../../DataBase/serviciosDB/imagenes';
+register();
 
 @Component({
   selector: 'app-galeria',
   standalone: true,
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   imports: [],
-  templateUrl: './galeria.component.html',
+  template:  `
+  <h2>GALERIA</h2>
+  <swiper-container class="swiper-galeria" init=false>
+  @for (swiperObject of swiperObjects; track $index) {
+    <swiper-slide class="slide-galeria"><div class="slide-custom">
+      <img [src]="swiperObject" alt="">
+    </div></swiper-slide>
+  }
+  </swiper-container>
+  `,
   styleUrl: './galeria.component.css'
 })
 export class GaleriaComponent {
+  swiperElements = signal<SwiperContainer | null>(null);
+  swiperObjects: string[] = galeriaImagenes;
 
+  ngOnInit(): void {
+    const swiperElemConstructor = document.querySelector('.swiper-galeria');
+    const swiperOptions: SwiperOptions = {
+      navigation:{
+        enabled:true,
+        nextEl:'.swiper-button-next',
+        prevEl:'.swiper-button-prev',
+      },
+      slidesPerView: 'auto',
+      speed: 1500,
+      breakpoints: {
+        0:{
+          slidesPerView:1,
+        },
+        800:{
+          slidesPerView:3,
+        }
+      },
+      spaceBetween:50,
+      effect:"coverflow",
+      coverflowEffect:{
+        slideShadows:false,
+      },
+      loop:true,
+      autoplay:true,
+      pagination:true
+    };
+    Object.assign(swiperElemConstructor!, swiperOptions);
+    this.swiperElements.set(swiperElemConstructor as SwiperContainer);
+    this.swiperElements()?.initialize();
+  }
 }
