700813c396d842db12c4e9c50b650eb1f77cbe1c
knarfangelo <knarf2003angelo@gmail.com>
2024-06-30 06:30:42 -0500

    nosotros arreglado

--- src/app/components/nosotros/nosotros.component.ts
+++ src/app/components/nosotros/nosotros.component.ts
@@ -10,31 +10,27 @@ register();
   schemas:[CUSTOM_ELEMENTS_SCHEMA],
   imports: [],
   template: `
-
     <header>
-      <img class="imagen-fondo" src="/nosotros/nosotrosfondo.jpg" alt="">
-    <div class="espacio"></div>
-    <div class="contenido">
-      <div class="nosotros">
-          <h2>NOSOTROS</h2>
-          <p>Somos Preciso Industry, empresa peruana de alta ingeniería que ofrece soluciones en media y baja tensión, redes, alumbrado y mantenimiento de subestaciones. Con más de 10 años de experiencia, nos especializamos en planificación, diseño y ejecución de proyectos eléctricos, garantizando cumplimiento en alcance, tiempo y costo.</p>
-      </div>
-     
-      <swiper-container class="swiper-nosotros">
+      <main>
+      <h2>NOSOTROS</h2>
+      <p>Somos Preciso Industry, empresa peruana de alta ingeniería que ofrece soluciones en media y baja tensión, redes, alumbrado y mantenimiento de subestaciones. Con más de 10 años de experiencia, nos especializamos en planificación, diseño y ejecución de proyectos eléctricos, garantizando cumplimiento en alcance, tiempo y costo.</p> 
+      <swiper-container init=false class="swiper-nosotros">
         @for (item of swiperObjects; track $index) {
           <swiper-slide>
-          <div class="slide-custom">
             <img class="imagen-slide" [src]="item.img" alt="">
             <h2>{{item.title}}</h2>
             <p>{{item.description}}</p>
-          </div>
         </swiper-slide> 
-      }
+      }    
     </swiper-container>
-    <div class="caja-boton">
+     <button class="swiper-button-prev"> < </button>
+      <button class="swiper-button-next"> > </button>
+    <div class="caja-button">
       <button>Saber más</button>
-    </div>      
+
     </div>
+
+    </main>
 
   </header>
   `,
@@ -42,31 +38,32 @@ register();
 })
 export class NosotrosComponent {
   swiperElements = signal<SwiperContainer | null>(null);
-  swiperObjects: INosotros[] = nosotros;
+  swiperObjects: INosotros[] = nosotros ;
 
   ngOnInit(): void {
     const swiperElemConstructor = document.querySelector('.swiper-nosotros');
     const swiperOptions: SwiperOptions = {
+      pagination:true,
       navigation:{
         enabled:true,
         nextEl:'.swiper-button-next',
         prevEl:'.swiper-button-prev',
       },
-      slidesPerView: 2.5,
-      speed: 1500,
+      slidesPerView: 1,
       breakpoints: {
         0:{
           slidesPerView:1,
         },
         800:{
+          slidesPerView:1,
+        },
+        1200:{
           slidesPerView:3,
         }
       },
-      spaceBetween:50,
-      effect:"",
-      loop:true,
+      spaceBetween:100,
+      effect:"slide",
       autoplay:true,
-      pagination:true
     };
     Object.assign(swiperElemConstructor!, swiperOptions);
     this.swiperElements.set(swiperElemConstructor as SwiperContainer);
