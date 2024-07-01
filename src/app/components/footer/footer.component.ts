133d6264985fb9c20c01853d7fe4a23f58f03c6e
knarfangelo <knarf2003angelo@gmail.com>
2024-07-01 17:00:13 -0500

    Terminado al 99%

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+
+  isDetailsOpen: boolean = true; // Variable que controla el estado de los detalles
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.setDetailsOpenState(event.target.innerWidth);
+  }
+
+  private setDetailsOpenState(width: number) {
+    this.isDetailsOpen = width > 800;
+  }
+
+  constructor() {
+    // Inicializar el estado inicial basado en el ancho actual
+    this.setDetailsOpenState(window.innerWidth);
+  }
 
 }
