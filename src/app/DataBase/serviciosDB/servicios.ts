00631d76f3bb3449f38f629d5e118dad25dc8b4c
knarfangelo <knarf2003angelo@gmail.com>
2024-07-02 11:45:58 -0500

    publicacion 1

--- src/app/DataBase/serviciosDB/servicios.ts
+++ src/app/DataBase/serviciosDB/servicios.ts
@@ -19,10 +19,29 @@ export const servicios: IServicio[] = [
                 subServicio: []
             },
         ]
+    }, 
+    {
+        img: '/servicios/3.jpg',
+        title: 'Tendido de Redes para habilitaciones urbanas',
+        description: 'En Preciso, realizamos instalaciones nuevas y solucionamos problemas del servicio de tendido de redes.',
+        subServicio: [
+            {
+                img: "/servicios/3.jpg",
+                title: "Diseño y planificación",
+                description: "Realizamos un estudio exhaustivo del terreno y las necesidades del cliente para diseñar redes eléctricas optimizadas.",
+                subServicio: []
+            },
+            {
+                img: "/servicios/3.jpg",
+                title: "Instalación de cables y equipos",
+                description: "Instalamos cables de alta calidad y equipos necesarios para el correcto funcionamiento de la red eléctrica, garantizando la seguridad y eficiencia del sistema.",
+                subServicio: []
+            }
+        ]
     },
     {
         img: '/servicios/2.jpg',
-        title: 'Mantenimiento de Subestaciones Eléctricas',
+        title: 'Soluciones para movilidad eléctrica',
         description: 'Brindamos servicios integrales de mantenimiento para subestaciones eléctricas, garantizando su óptimo funcionamiento y seguridad.',
         subServicio: [
             {
@@ -38,7 +57,7 @@ export const servicios: IServicio[] = [
                 subServicio: []
             },
         ]
-    },
+    }, 
     {
         img: '/servicios/3.jpg',
         title: 'Servicios Eléctricos de Baja Tensión',
@@ -59,9 +78,30 @@ export const servicios: IServicio[] = [
         ]
     },
     {
+        img: '/servicios/2.jpg',
+        title: 'Mantenimiento de Subestaciones Eléctricas',
+        description: 'Brindamos servicios integrales de mantenimiento para subestaciones eléctricas, garantizando su óptimo funcionamiento y seguridad.',
+        subServicio: [
+            {
+                img: "/servicios/2.jpg",
+                title: "Mantenimiento Preventivo",
+                description: "Realizamos maniobras de desconexión, limpieza del transformador y celda, mediciones de temperatura y aislamiento, ajustes de terminales y relés de protección; y saneamiento con solvente dieléctrico.",
+                subServicio: []
+            },
+            {
+                img: "/servicios/2.jpg",
+                title: "Mantenimiento Correctivo",
+                description: "Diagnosticamos y corregimos fallos en subestaciones eléctricas, proporcionando informes técnicos detallados y certificados de operatividad firmados por ingenieros eléctricos colegiados.",
+                subServicio: []
+            },
+        ]
+    },
+   
+   
+    {
         img: '/servicios/3.jpg',
-        title: 'Tendido de Redes',
-        description: 'Planificamos, instalamos y mantenemos redes eléctricas para una distribución segura y eficiente de la energía eléctrica.',
+        title: 'Servicio de mantenimiento de tableros eléctricos',
+        description: 'Realizamos el mantenimiento rutinario de los tableros eléctricos como la forma ideal de garantizar el buen funcionamiento de las instalaciones eléctricas. Estos dispositivos son fundamentales para el normal funcionamiento de los fluidos eléctricos en plantas industriales, residenciales, de viviendas y comerciales.',
         subServicio: [
             {
                 img: "/servicios/3.jpg",
@@ -76,5 +116,33 @@ export const servicios: IServicio[] = [
                 subServicio: []
             }
         ]
-    }
+    }, 
+    {
+        img: '/servicios/3.jpg',
+        title: 'Pozos a tierra',
+        description: 'En Preciso, te ofrecemos todo el detalle del diseño técnico del pozo a tierra (tipo de varilla, características, profundidad, camas que se han realizado de tierras, dimensiones de las varillas con el conector A/B, etc). Además, los certificados que emitimos son válidos para INDECI, auditorías y para obtener la licencia de funcionamiento.',
+        subServicio: [
+            {
+                img: "/servicios/3.jpg",
+                title: "Diseño y planificación",
+                description: "Realizamos un estudio exhaustivo del terreno y las necesidades del cliente para diseñar redes eléctricas optimizadas.",
+                subServicio: []
+            },
+            {
+                img: "/servicios/3.jpg",
+                title: "Instalación de cables y equipos",
+                description: "Instalamos cables de alta calidad y equipos necesarios para el correcto funcionamiento de la red eléctrica, garantizando la seguridad y eficiencia del sistema.",
+                subServicio: []
+            }
+        ]
+    },
+    {
+        img: '/servicios/3.jpg',
+        title: 'Otros servicios',
+        description: '',
+        subServicio: [
+           
+        ]
+    },
+   
 ]
